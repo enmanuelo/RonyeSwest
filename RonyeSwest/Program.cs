d868c3a40f82edfada21b51abd63dceb78c43c40
Enmanuel Ozoria <ozoriaeman1998@gmail.com>
2022-08-04 09:31:41 -0400

    Created conversation between Kanye and Ron

---
+++ RonyeSwest/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using RonyeSwest;
+
+
+for (int i = 0; i < 10; i++)
+{
+    QuoteMachine.YeQuote();
+    QuoteMachine.RonQuote();
+}
---
+++ RonyeSwest/QuoteMachine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace RonyeSwest
+{
+	public class QuoteMachine
+	{
+		public static void YeQuote()
+		{
+			var client = new HttpClient();
+
+			var kanyeURL = "https://api.kanye.rest/";
+
+			var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+			var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("qoute").ToString();
+
+			Console.WriteLine($"Ye: {kanyeQuote}\n");
+
+		}
+
+		public static void RonQuote()
+		{
+			var client = new HttpClient();
+
+			var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+			var ronResponse = client.GetStringAsync(ronURL).Result;
+
+			var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+			Console.WriteLine($"Ron: {ronQuote}\n");
+
+		}
+	}
+}
+
